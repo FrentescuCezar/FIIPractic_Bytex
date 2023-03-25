70eb33ec04c9acc4547e6c14e6ca50de26eea264
cezar <79227201+FrentescuCezar@users.noreply.github.com>
2023-03-25 20:23:23 +0200

    Egg Hatching

--- week3/starter/pokemonservices/pokemoncatalog/src/main/java/com/fiipractic/pokemoncatalog/model/Pokemon.java
+++ week3/starter/pokemonservices/pokemoncatalog/src/main/java/com/fiipractic/pokemoncatalog/model/Pokemon.java
@@ -5,11 +5,21 @@ public class Pokemon {
     private Integer id;
     private String name;
     private Integer baseTotal;
+    private Integer baseEggSteps;
 
-    public Pokemon(Integer id, String name, Integer baseTotal) {
+    public Pokemon(Integer id, String name, Integer baseTotal, Integer baseEggSteps) {
         this.id = id;
         this.name = name;
         this.baseTotal = baseTotal;
+        this.baseEggSteps = baseEggSteps;
+    }
+
+    public Integer getBaseEggSteps() {
+        return baseEggSteps;
+    }
+
+    public void setBaseEggSteps(Integer baseEggSteps) {
+        this.baseEggSteps = baseEggSteps;
     }
 
     public Pokemon() {
@@ -45,6 +55,7 @@ public class Pokemon {
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", baseTotal=" + baseTotal +
+                ", baseEggSteps=" + baseEggSteps +
                 '}';
     }
 }
