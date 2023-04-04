da4304de5fd151398aa36fb3fcc5b3329e25d7e8
cezar <79227201+FrentescuCezar@users.noreply.github.com>
2023-04-04 15:09:18 +0300

    Added a new "nameJWTExtraction" just in case

--- week3/starter/pokemonservices/comment/src/main/java/com/fiipractic/comment/utils/ExtractJWT.java
+++ week3/starter/pokemonservices/comment/src/main/java/com/fiipractic/comment/utils/ExtractJWT.java
@@ -38,4 +38,36 @@ public class ExtractJWT {
 
         return null;
     }
+
+    public static String nameJWTExtraction(String token, String extraction) {
+        token = token.replace("Bearer", "").trim();
+
+        String[] split_string = token.split("\\.");
+        Base64.Decoder decoder = Base64.getUrlDecoder();
+
+        String payload = new String(decoder.decode(split_string[1]));
+
+        String[] entries = payload.split(",");
+        Map<String, String> map = new HashMap<String, String>();
+
+        for (String entry : entries) {
+            String[] keyValue = entry.split(":");
+
+            // Remove quotes and trim the key
+            String key = keyValue[0].replace("\"", "").trim();
+
+            if (key.equals(extraction)) {
+                // Remove quotes, braces, and trim the value
+                String value = keyValue[1].replace("\"", "").replace("}", "").trim();
+
+                map.put(key, value);
+            }
+        }
+
+        if (map.containsKey(extraction)) {
+            return map.get(extraction);
+        }
+
+        return null;
+    }
 }
