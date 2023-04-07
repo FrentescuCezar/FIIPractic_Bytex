9d09fdb1ed30df9e4cbbd44abc75d5f8cac286dc
cezar <79227201+FrentescuCezar@users.noreply.github.com>
2023-04-07 06:01:03 +0300

    Deleted stuff + Backup Database

---
+++ Backup Baza De Date/_comment__202304070556.sql
@@ -0,0 +1,18 @@
+INSERT INTO public."comment" ("date",pokemon_id,rating,comment_description,user_email) VALUES
+	 ('2023-04-03 22:27:03.801',1,5.0,'hey baby','frentescucezar@gmail.com'),
+	 ('2023-04-04 03:38:25.361',2,5.0,'Test daca e bun portul asta','frentescucezar@gmail.com'),
+	 ('2023-04-04 03:49:53.395',3,5.0,'Test daca e bun portul asta','frentescucezar@gmail.com'),
+	 ('2023-04-04 03:50:06.423',4,4.0,'HAI BAAA MERGI','frentescucezar@gmail.com'),
+	 ('2023-04-04 04:17:05.42',24,3.5,'ce gay','frentescucezar@gmail.com'),
+	 ('2023-04-02 02:49:54.006539',1,3.0,'sal','user1@example.com'),
+	 ('2023-04-04 14:00:47.427',37,3.0,'wannabe samurai jack','frentescucezar@gmail.com'),
+	 ('2023-04-04 04:19:29.292',23,4.5,'The pokemon is okay, but could have been better.','frentescucezar@gmail.com'),
+	 ('2023-04-02 02:49:54.010785',2,3.0,'taci drq','user2@example.com'),
+	 ('2023-04-04 22:30:00.865',34,3.0,'HEY','frentescucezar@gmail.com');
+INSERT INTO public."comment" ("date",pokemon_id,rating,comment_description,user_email) VALUES
+	 ('2023-04-05 01:23:32.072',30,1.5,'Gustos','frentescucezar@gmail.com'),
+	 ('2023-04-05 06:14:47.469',51,5.0,'Primul meu pokemon incarcat de pe site direct in baza de date. 5/5 CLAR ♥♥♥♥♥♥♥♥♥','frentescucezar@gmail.com'),
+	 ('2023-04-04 03:45:10.27',5,0.0,'Sonic','frentescucezar@gmail.com'),
+	 ('2023-04-04 03:49:53.395',3,5.0,'Test daca e bun portul asta','testcomentariu@gmail.com'),
+	 ('2023-04-05 22:47:31.337',9,2.5,'add comment','frentescucezar@gmail.com'),
+	 ('2023-04-05 22:47:52.609',16,1.5,'add comment baby','frentescucezar@gmail.com');
