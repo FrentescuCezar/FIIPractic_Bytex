745560fe0c04145531c70f8a7c6f2957adb73a2e
cezar <79227201+FrentescuCezar@users.noreply.github.com>
2023-04-08 03:22:53 +0300

    Recent Database Backup

---
+++ Backup Baza De Date/_comment__202304080258.sql
@@ -0,0 +1,15 @@
+INSERT INTO public."comment" ("date",pokemon_id,rating,comment_description,user_name) VALUES
+	 ('2023-04-07 17:50:54.693',62,4.5,'Vilgax want to know your location','frentescucezar@gmail.com'),
+	 ('2023-04-07 17:52:08.935',66,3.5,'Magikarp''s bully.','frentescucezar@gmail.com'),
+	 ('2023-04-07 17:52:39.13',64,4.5,'Evil fart.','frentescucezar@gmail.com'),
+	 ('2023-04-05 06:14:47.469',51,5.0,'Primul meu pokemon incarcat de pe site direct in baza de date. 5/5 CLAR ♥♥♥♥♥♥♥♥♥','FrentescuCezar'),
+	 ('2023-04-07 17:46:23.008',53,4.0,'Lolipop, Lolipop, Lolipop.','FrentescuCezar'),
+	 ('2023-04-07 17:46:05.22',60,4.0,'ce gay','FrentescuCezar'),
+	 ('2023-04-07 17:49:08.574',63,5.0,'banana1235','FrentescuCezar'),
+	 ('2023-04-07 17:48:01.027',68,2.5,'wtf "Joy that came from the discovery of the lollipop and the banana tree."','FrentescuCezar'),
+	 ('2023-04-07 19:20:43.449',75,4.5,'Nice nose bro.','FrentescuCezar'),
+	 ('2023-04-07 19:42:30.029',74,5.0,'Bro shave your Hairy Chest!','FrentescuCezar');
+INSERT INTO public."comment" ("date",pokemon_id,rating,comment_description,user_name) VALUES
+	 ('2023-04-07 19:47:27.649',75,4.5,'Farts are his biggest enemy.','KokoGorilla'),
+	 ('2023-04-08 01:24:38.761',65,3.5,'Fireboy And Watergirl Light Temple 
+Y8.com.','FrentescuCezar');
