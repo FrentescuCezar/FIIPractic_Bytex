dba4206ff0918fca2b71cb0a3e8c15037931204b
cezar <79227201+FrentescuCezar@users.noreply.github.com>
2023-04-14 22:28:54 +0300

    Whos.that.pokemon Refactored

--- Poketex/02-backend/starter/pokemonservices/whos.that.pokemon/src/main/java/com/fiipractic/whos/that/pokemon/controller/WhosThatPokemonController.java
+++ Poketex/02-backend/starter/pokemonservices/whos.that.pokemon/src/main/java/com/fiipractic/whos/that/pokemon/controller/WhosThatPokemonController.java
@@ -1,31 +1,27 @@
 package com.fiipractic.whos.that.pokemon.controller;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
+import java.util.HashMap;
+import java.util.Map;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fiipractic.whos.that.pokemon.service.WhosThatPokemonService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
 
-@RestController
-@JsonFormat
 @CrossOrigin(origins = "http://localhost:3000")
+@RestController
 public class WhosThatPokemonController {
 
-
     private final WhosThatPokemonService whosThatPokemonService;
-    private final ObjectMapper objectMapper;
 
     @Autowired
     public WhosThatPokemonController(WhosThatPokemonService whosThatPokemonService) {
         this.whosThatPokemonService = whosThatPokemonService;
-        this.objectMapper = new ObjectMapper();
     }
 
     @GetMapping(value = "/")
@@ -35,32 +31,10 @@ public class WhosThatPokemonController {
     }
 
     @PostMapping(value = "/guess", produces = MediaType.APPLICATION_JSON_VALUE)
-    public String guessPokemon(@RequestParam String guess, @RequestParam Integer pokemonIndex) throws JsonProcessingException {
-        String pokemon = whosThatPokemonService.getPokemonById(pokemonIndex);
-        JsonNode pokemonNode = objectMapper.readTree(pokemon);
-
-        String result;
-        String[] promptArray = pokemonNode.get("prompt").asText().split(" ");
-
-        boolean isGuessCorrect = false;
-
-        if (guess.equalsIgnoreCase(pokemonNode.get("name").asText()))
-            isGuessCorrect = true;
-
-        for (String promptWord : promptArray)
-            if (guess.equalsIgnoreCase(promptWord)) {
-                isGuessCorrect = true;
-                break;
-            }
-
-        if (isGuessCorrect)
-            result = "Correct!";
-        else
-            result = "Wrong!";
-
+    public Map<String, Object> guessPokemon(@RequestParam String guess, @RequestParam Integer pokemonIndex) throws JsonProcessingException {
         Map<String, Object> response = new HashMap<>();
+        String result = whosThatPokemonService.checkGuess(guess, pokemonIndex);
         response.put("result", result);
-
-        return objectMapper.writeValueAsString(response);
+        return response;
     }
 }
--- Poketex/02-backend/starter/pokemonservices/whos.that.pokemon/src/main/java/com/fiipractic/whos/that/pokemon/service/WhosThatPokemonService.java
+++ Poketex/02-backend/starter/pokemonservices/whos.that.pokemon/src/main/java/com/fiipractic/whos/that/pokemon/service/WhosThatPokemonService.java
@@ -4,20 +4,32 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
+import java.util.List;
+
 
 @Service
 public class WhosThatPokemonService {
-    public WhosThatPokemonService() {
+    private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
+    private final String poketexUrl;
+
+    @Autowired
+    public WhosThatPokemonService(RestTemplate restTemplate,
+                                  ObjectMapper objectMapper,
+                                  @Value("${api.base-url.poketexUrl}") String poketexUrl) {
+        this.restTemplate = restTemplate;
+        this.objectMapper = objectMapper;
+        this.poketexUrl = poketexUrl;
     }
 
     public String getRandomPokemon() throws JsonProcessingException {
-
-         RestTemplate restTemplate = new RestTemplate();
-         ObjectMapper objectMapper = new ObjectMapper();
 
         int limit = 1;
         String url = "http://localhost:8084/api/poketex/random?limit=" + limit;
@@ -30,11 +42,37 @@ public class WhosThatPokemonService {
     }
 
     public String getPokemonById(Integer pokemonId) throws JsonProcessingException {
-        RestTemplate restTemplate = new RestTemplate();
-        ObjectMapper objectMapper = new ObjectMapper();
 
         String url = "http://localhost:8084/api/poketexes/" + pokemonId;
         ResponseEntity<JsonNode> response = restTemplate.getForEntity(url, JsonNode.class);
         return objectMapper.writeValueAsString(response.getBody());
     }
+
+    public String checkGuess(String guess, Integer pokemonId) throws JsonProcessingException {
+        String pokemon = getPokemonById(pokemonId);
+        JsonNode pokemonNode = objectMapper.readTree(pokemon);
+
+        String result;
+        String[] promptArray = pokemonNode.get("prompt").asText().split(" ");
+
+        boolean isGuessCorrect = false;
+
+        if (guess.equalsIgnoreCase(pokemonNode.get("name").asText()))
+            isGuessCorrect = true;
+
+        for (String promptWord : promptArray) {
+            if (guess.equalsIgnoreCase(promptWord)) {
+                isGuessCorrect = true;
+                break;
+            }
+        }
+
+        if (isGuessCorrect)
+            result = "Correct!";
+        else
+            result = "Wrong!";
+
+        return result;
+    }
+
 }
